13913aa1517526639409e39a156ea55efd970538
Haris Daffa <harisdaffa33@gmail.com>
2026-04-25 10:41:54 +0700

    feat: user table with dummy data

--- src/app/features/dashboard/components/user-table/user-table.component.ts
+++ src/app/features/dashboard/components/user-table/user-table.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.scss']
+  styleUrls: ['./user-table.component.scss'],
 })
 export class UserTableComponent implements OnInit {
+  users = [
+    { firstName: 'Mark', lastName: 'Otto', username: '@mdo' },
+    { firstName: 'Jacob', lastName: 'Throton', username: '@fat' },
+    { firstName: 'Larry', lastName: 'theBird', username: '@twitter' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
